780db5187d407b80fd0a95241a1633b493759e44
James Aslett <jamesaslett1985@gmail.com>
2017-01-13 22:23:07 +0000

    background cover

--- js/navscripts.js
+++ js/navscripts.js
@@ -35,20 +35,11 @@ $('#myTopnav li a, .mojNavtext').on('click', function(){
     $('.hamburger').removeClass(" is-active");
 });
 
+/* Hide nav if user clicks outside of it
 
+$(".responsive").bind( "clickoutside", function(event){
+$(this).hide();
 
-
-
-$(document).ready(function() {
-var movementStrength = 25;
-var height = movementStrength / $(window).height();
-var width = movementStrength / $(window).width();
-$(".moj-logo").mousemove(function(e){
-          var pageX = e.pageX - ($(window).width() / 2);
-          var pageY = e.pageY - ($(window).height() / 2);
-          var newvalueX = width * pageX * -1 - 25;
-          var newvalueY = height * pageY * -1 - 50;
-          $('.moj-logo').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
-});
+*/
 

1ff0400cd4150796160cc3e7248982558575d245
jamesaslett1985 <jamesaslett1985@gmail.com>
2017-01-25 12:58:58 +0000

    Renamed IDs and Classes to be generic

--- js/navscripts.js
+++ js/navscripts.js
@@ -1,47 +1,46 @@
 //TOP NAV JQuery
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("topnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
-}	
-	
+}
+
 //Toggle between hamburger/cross icons on click
 
 var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
+$hamburger.on("click", function (e) {
     $hamburger.toggleClass("is-active");
-  });
+});
 
 
 //Force mobile menu to shut if left open on screen resize
-$(window).resize(function(){
-	if($(window).width() > 760) {
-//Remove Responsive class
-	$("#myTopnav").removeClass(" responsive");
-//Force X icon back to hamburger
-	var a = $(".hamburger");
-	a.removeClass(" is-active");
-    } 
+$(window).resize(function () {
+    if ($(window).width() > 760) {
+        //Remove Responsive class
+        $("#topnav").removeClass(" responsive");
+        //Force X icon back to hamburger
+        var a = $(".hamburger");
+        a.removeClass(" is-active");
+    }
 });
 
 //Force mobile menu to shut if any option selected
-$('#myTopnav li a, .mojNavtext').on('click', function(){
-    $('#myTopnav').removeClass('responsive');
-//Force X icon back to hamburger
+$('#topnav li a, .nav-text').on('click', function () {
+    $('#topnav').removeClass('responsive');
+    //Force X icon back to hamburger
     $('.hamburger').removeClass(" is-active");
 });
 
 // Hide nav if user clicks outside of it
-$(document).on('click', function(event) {
-  if (!$(event.target).closest('.hamburger-box').length) {
-    // Hide mobile menu
-    $('#myTopnav').removeClass('responsive');
-    // Force X icon back to hamburger
-    $('.hamburger').removeClass(" is-active");
-  }
+$(document).on('click', function (event) {
+    if (!$(event.target).closest('.hamburger-box').length) {
+        // Hide mobile menu
+        $('#topnav').removeClass('responsive');
+        // Force X icon back to hamburger
+        $('.hamburger').removeClass(" is-active");
+    }
 })
-

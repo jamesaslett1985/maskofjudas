9868f78bdd781667019589338a14a578ff50fed8
jamesaslett1985 <jamesaslett1985@gmail.com>
2017-02-25 00:18:45 +0000

    Added UPDATE page

--- gigsjson.php
+++ gigsjson.php
@@ -1,5 +1,5 @@
 <?php
 $jsondata = file_get_contents("php://input");
 echo $jsondata;
-$writeJson = file_put_contents('data.json', $jsondata);
+$writeJson = file_put_contents('gigs.json', $jsondata);
 ?>
--- js/gigs.js
+++ js/gigs.js
@@ -1,6 +1,27 @@
 function saveData(){
 
-$.post("gigsjson.php", {"test": "value"})
+$.post("gigsjson.php", {  
+   "gigs":[  
+      {  
+         "gig":"BRIGHTON ELECTRIC 1ST FEBRUARY 2017 7:30PM"
+      },
+      {  
+         "gig":"BRIGHTON ELECTRIC 2ND FEBRUARY 2017 7:30PM"
+      },
+      {  
+         "gig":"BRIGHTON ELECTRIC 3RD FEBRUARY 2017 7:30PM"
+      },
+      {  
+         "gig":"BRIGHTON ELECTRIC 4TH FEBRUARY 2017 7:30PM"
+      },
+      {  
+         "gig":"BRIGHTON ELECTRIC 5TH FEBRUARY 2017 7:30PM"
+      },
+      {  
+         "gig":"BRIGHTON ELECTRIC 6TH FEBRUARY 2017 7:30PM"
+      }
+   ]
+})
     .done(function(data){
         console.log(data);
     })

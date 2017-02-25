6ccf1bae0566293b0240c4b24e15ce05e08b066a
jamesaslett1985 <jamesaslett1985@gmail.com>
2017-02-25 23:28:24 +0000

    ajax script update

--- js/gigs.js
+++ js/gigs.js
@@ -1,26 +1,29 @@
 function saveData(){
 
-$.post("gigsjson.php", {  
-   "gigs":[  
-      {  
-         "gig":"BRIGHTON ELECTRIC 1ST FEBRUARY 2017 7:30PM"
-      },
-      {  
-         "gig":"BRIGHTON ELECTRIC 2ND FEBRUARY 2017 7:30PM"
-      },
-      {  
-         "gig":"BRIGHTON ELECTRIC 3RD FEBRUARY 2017 7:30PM"
-      },
-      {  
-         "gig":"BRIGHTON ELECTRIC 4TH FEBRUARY 2017 7:30PM"
-      },
-      {  
-         "gig":"BRIGHTON ELECTRIC 5TH FEBRUARY 2017 7:30PM"
-      },
-      {  
-         "gig":"BRIGHTON ELECTRIC 6TH FEBRUARY 2017 7:30PM"
-      }
-   ]
+$.ajax({
+    type: "POST",
+    url: "gigsjson.php",
+    contentType:"application/json",  
+    data:   
+            {  
+                "gig":"BRIGHTON ELECTRIC 1ST FEBRUARY 2017 7:30PM"
+            },
+            {  
+                "gig":"BRIGHTON ELECTRIC 2ND FEBRUARY 2017 7:30PM"
+            },
+            {  
+                "gig":"BRIGHTON ELECTRIC 3RD FEBRUARY 2017 7:30PM"
+            },
+            {  
+                "gig":"BRIGHTON ELECTRIC 4TH FEBRUARY 2017 7:30PM"
+            },
+            {  
+                "gig":"BRIGHTON ELECTRIC 5TH FEBRUARY 2017 7:30PM"
+            },
+            {  
+                "gig":"BRIGHTON ELECTRIC 6TH FEBRUARY 2017 7:30PM"
+            }
+        
 })
     .done(function(data){
         console.log(data);
